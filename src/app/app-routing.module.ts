48f97c6cc6b37f27b53f0830edba5f2aebbd8985
ChrisNguyen <kiennt.6795@gmail.com>
2024-04-11 21:07:50 +0700

    update authen guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,31 +2,88 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { NgSelectModule } from '@ng-select/ng-select';
 import { LoginGuard } from './core/guards/login.guard';
+import { AuthenticatedGuard } from './core/guards/authenticated.guard';
 
 const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
-  { path: 'home',  loadChildren: () => import('./home/home.module').then(x => x.HomePageModule) },
+  {
+    path: 'home',
+    loadChildren: () =>
+      import('./home/home.module').then((x) => x.HomePageModule),
+    canActivate: [AuthenticatedGuard],
+  },
   {
     path: 'login',
-    loadChildren: () => import('./login/login.module').then(x => x.LoginPageModule),
-    canLoad: [LoginGuard]
+    loadChildren: () =>
+      import('./login/login.module').then((x) => x.LoginPageModule),
+    canLoad: [LoginGuard],
   },
-  { path: 'signup', loadChildren: () => import('./signup/signup.module').then(x => x.SignupPageModule) },
+  {
+    path: 'signup',
+    loadChildren: () =>
+      import('./signup/signup.module').then((x) => x.SignupPageModule),
+  },
   // { path: 'passwordreset', loadChildren: () => import('./passwordreset/passwordreset.module').then(x => x.PasswordresetPageModule) },
   // { path: 'list', loadChildren: () => import('./list/list.module').then(x => x.ListPageModule) },
   // { path: 'infomodal', loadChildren: () => import('./infomodal/infomodal.module').then(x => x.InfomodalPageModule) },
-  { path: 'cart', loadChildren: () => import('./cart/cart.module').then(x => x.CartPageModule) },
-  { path: 'productdetail/:id', loadChildren: () => import('./productdetail/productdetail.module').then(x => x.ProductdetailPageModule) },
-  { path: 'productlist', loadChildren: () => import('./productlist/productlist.module').then(x => x.ProductlistPageModule) },
-  { path: 'search', loadChildren: () => import('./search/search.module').then(x => x.SearchPageModule) },
+  {
+    path: 'cart',
+    loadChildren: () =>
+      import('./cart/cart.module').then((x) => x.CartPageModule),
+    canActivate: [AuthenticatedGuard],
+  },
+  {
+    path: 'productdetail/:id',
+    loadChildren: () =>
+      import('./productdetail/productdetail.module').then(
+        (x) => x.ProductdetailPageModule
+      ),
+    canActivate: [AuthenticatedGuard],
+  },
+  {
+    path: 'productlist',
+    loadChildren: () =>
+      import('./productlist/productlist.module').then(
+        (x) => x.ProductlistPageModule
+      ),
+    canActivate: [AuthenticatedGuard],
+  },
+  {
+    path: 'search',
+    loadChildren: () =>
+      import('./search/search.module').then((x) => x.SearchPageModule),
+    canActivate: [AuthenticatedGuard],
+  },
   // { path: 'notification', loadChildren: () => import('./notification/notification.module').then(x => x.NotificationPageModule) },
   // { path: 'NewAddress/:id', loadChildren: () => import('./new-address/new-address.module').then(x => x.NewAddressPageModule) },
-  { path: 'Checkout', loadChildren: () => import('./checkout/checkout.module').then(x => x.CheckoutPageModule) },
+  {
+    path: 'Checkout',
+    loadChildren: () =>
+      import('./checkout/checkout.module').then((x) => x.CheckoutPageModule),
+    canActivate: [AuthenticatedGuard],
+  },
   // { path: 'wishcash', loadChildren: () => import('./wishcash/wishcash.module').then(x => x.WishcashPageModule) },
   // { path: 'applypromo', loadChildren: () => import('./applypromo/applypromo.module').then(x => x.ApplypromoPageModule) },
-  { path: 'orders', loadChildren: () => import('./orders/orders.module').then(x => x.OrdersPageModule) },
-  { path: 'customers', loadChildren: () => import('./customers/customers.module').then(x => x.CustomersPageModule) },
-  { path: 'doanh-thu', loadChildren: () => import('./commission/commission.module').then(x => x.CommissionPageModule) },
+  {
+    path: 'orders',
+    loadChildren: () =>
+      import('./orders/orders.module').then((x) => x.OrdersPageModule),
+    canActivate: [AuthenticatedGuard],
+  },
+  {
+    path: 'customers',
+    loadChildren: () =>
+      import('./customers/customers.module').then((x) => x.CustomersPageModule),
+    canActivate: [AuthenticatedGuard],
+  },
+  {
+    path: 'doanh-thu',
+    loadChildren: () =>
+      import('./commission/commission.module').then(
+        (x) => x.CommissionPageModule
+      ),
+    canActivate: [AuthenticatedGuard],
+  },
   // { path: 'orderinfo', loadChildren: () => import('./orderinfo/orderinfo.module').then(x => x.OrderinfoPageModule) },
   // { path: 'faqs', loadChildren: () => import('./faqs/faqs.module').then(x => x.FaqsPageModule) },
   // { path: 'faq', loadChildren: () => import('./faq/faq.module').then(x => x.FaqPageModule) },
@@ -46,17 +103,53 @@ const routes: Routes = [
   // { path: 'country', loadChildren: () => import('./country/country.module').then(x=>x.CountryPageModule) },
   // { path: 'changeemail', loadChildren: () => import('./changeemail/changeemail.module').then(x => x.ChangeemailPageModule) },
   // { path: 'changepassword', loadChildren: () => import('./changepassword/changepassword.module').then(x=>x.ChangepasswordPageModule) },
-  { path: 'account', loadChildren: () => import('./account/account.module').then(x=>x.AccountPageModule) },
-  { path: 'account-edit',loadChildren: () => import('./account-edit/account-edit.module').then( m => m.AccountEditPageModule)},
-  { path: 'account-delete',loadChildren: () => import('./account-delete/account-delete.module').then( m => m.AccountDeletePageModule)},
-  { path: 'account-point',loadChildren: () => import('./account-point/account-point.module').then( m => m.AccountPointPageModule)},
-  { path: 'point-history',loadChildren: () => import('./point-histories/point-histories.module').then( m => m.PointHistoriesPageModule)},
-  { path: 'info-app',loadChildren: () => import('./info-app/info-app.module').then( m => m.InfoAppPageModule)},
-
+  {
+    path: 'account',
+    loadChildren: () =>
+      import('./account/account.module').then((x) => x.AccountPageModule),
+    canActivate: [AuthenticatedGuard],
+  },
+  {
+    path: 'account-edit',
+    loadChildren: () =>
+      import('./account-edit/account-edit.module').then(
+        (m) => m.AccountEditPageModule
+      ),
+    canActivate: [AuthenticatedGuard],
+  },
+  {
+    path: 'account-delete',
+    loadChildren: () =>
+      import('./account-delete/account-delete.module').then(
+        (m) => m.AccountDeletePageModule
+      ),
+    canActivate: [AuthenticatedGuard],
+  },
+  {
+    path: 'account-point',
+    loadChildren: () =>
+      import('./account-point/account-point.module').then(
+        (m) => m.AccountPointPageModule
+      ),
+    canActivate: [AuthenticatedGuard],
+  },
+  {
+    path: 'point-history',
+    loadChildren: () =>
+      import('./point-histories/point-histories.module').then(
+        (m) => m.PointHistoriesPageModule
+      ),
+    canActivate: [AuthenticatedGuard],
+  },
+  {
+    path: 'info-app',
+    loadChildren: () =>
+      import('./info-app/info-app.module').then((m) => m.InfoAppPageModule),
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
 export class AppRoutingModule {}
--- src/app/core/guards/authenticated.guard.ts
+++ src/app/core/guards/authenticated.guard.ts
@@ -1,5 +1,11 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, CanActivateChild } from '@angular/router';
+import {
+  CanActivate,
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot,
+  UrlTree,
+  CanActivateChild,
+} from '@angular/router';
 import { from, Observable } from 'rxjs';
 import { map, take, tap } from 'rxjs/operators';
 import { AlertController, NavController } from '@ionic/angular';
@@ -9,29 +15,34 @@ import { Storage } from '@ionic/storage-angular';
 const TOKEN_KEY = 'auth-token';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthenticatedGuard implements CanActivate {
   constructor(
     private auth: AuthService,
     private navCtrl: NavController,
     private storage: Storage
-  ) { }
+  ) {}
   canActivate(
     next: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    state: RouterStateSnapshot
+  ):
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree>
+    | boolean
+    | UrlTree {
     const promiseSource = from(this.storage.get(TOKEN_KEY));
     return promiseSource.pipe(
-			take(1),
-			map((user) => {
-				if (!user) {
-					this.navCtrl.navigateRoot('./login');
-					return false;
-				} else {
-					return true;
-				}
-			})
-		);
+      take(1),
+      map((user) => {
+        if (!user) {
+          this.navCtrl.navigateRoot('/login');
+          return false;
+        } else {
+          return true;
+        }
+      })
+    );
   }
   // canActivateChild(
   //   next: ActivatedRouteSnapshot,
